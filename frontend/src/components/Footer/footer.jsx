4c6e3525431446228eaf11008abe1206913104d1
kumari01 <kumari.gadi@sasi.ac.in>
2026-03-15 12:29:10 +0530

    update footer

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -37,10 +37,7 @@ function footer(){
                     <ul className="footer-links">
                         <li><a href="#">SITE TDD</a></li>
                         <li><a href="https://wams-society.com" target="_blank" rel="noreferrer">WAMS Society</a></li>
-                        <li><a href="#">WAMS 2025</a></li>
-                        <li><a href="#">WAMS 2024</a></li>
-                        <li><a href="#">WAMS 2023</a></li>
-                        <li><a href="#">WAMS 2022</a></li>
+                        <li><a href="#">WAMS 2026</a></li>
                         <li><a href="#">Site map</a></li>
                     </ul>
                 </div>
@@ -51,11 +48,11 @@ function footer(){
                     <ul className="footer-links footer-contact">
                         <li>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 4-8 5-8-5V6l8 5 8-5z"/></svg>
-                            <a href="mailto:wams2027@site.ac.in">Email: wams2027@site.ac.in</a>
+                            <a href="mailto:Wams2027@sasi.ac.in">Email: Wams2027@sasi.ac.in</a>
                         </li>
                         <li>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><path d="M6.6 10.8c1.4 2.8 3.8 5.1 6.6 6.6l2.2-2.2c.3-.3.7-.4 1-.2 1.1.4 2.3.6 3.6.6.6 0 1 .4 1 1V20c0 .6-.4 1-1 1-9.4 0-17-7.6-17-17 0-.6.4-1 1-1h3.5c.6 0 1 .4 1 1 0 1.3.2 2.5.6 3.6.1.3 0 .7-.2 1z"/></svg>
-                            <a href="tel:+919492307681">+91 94923 07681</a>
+                            <a href="tel:+919492307681">+91 8074480076</a>
                         </li>
                         <li>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7m0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5"/></svg>
